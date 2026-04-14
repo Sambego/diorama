b8bd4a5a0d662056bec632ee06d4c3d4200598f6
Sambego <sambellen@gmail.com>
2026-04-14 15:01:49 +0200

    Pass styles to columns

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -5,13 +5,14 @@ import styles from './Columns.css';
 export interface ColumnsProps {
   children: React.ReactElement<{ className?: string; style?: React.CSSProperties }>[];
   className?: string;
+  style?: React.CSSProperties;
 }
 
-const Columns = ({ children, className = '' }: ColumnsProps) => {
+const Columns = ({ children, className = '', style }: ColumnsProps) => {
   const renderColumns = () => children.map((child, index) => cloneElement(child, {
     className: `${styles.column} diorama-column ${className} `,
     key: index,
-    style: { maxWidth: `${100 / children.length}%` },
+    style: { maxWidth: `${100 / children.length}%`, ...style },
   }));
 
   return <div className={`${styles.columns} diorama-columns`}>{renderColumns()}</div>;
